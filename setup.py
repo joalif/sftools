1b768a4835e1ae7b920e787350c60e3a99b3ddbe
Dan Streetman <ddstreet@ieee.org>
2022-03-08 11:22:46 -0500

    add setuptools files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='sftools',
+    version='1.0.0',
+    packages=['sftools'],
+    install_requires=[
+        'simple-salesforce',
+        'ipython',
+    ],
+)
