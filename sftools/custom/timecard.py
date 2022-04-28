8d8f486b434077810aa49848d2373b8ff7562f03
Dan Streetman <ddstreet@ieee.org>
2022-04-28 14:20:20 -0400

    add sf-timecard, add more to timecard module

--- sftools/custom/timecard.py
+++ sftools/custom/timecard.py
@@ -6,17 +6,36 @@ from functools import cached_property
 
 from sftools.case import SFCaseObject
 from sftools.object import SFObject
+from sftools.soql import WhereUtil
 from sftools.type import SFType
+from sftools.user import SFUserObject
 
 
 # Extend SFCaseObject with timecards()
-SFCaseObject.timecards = lambda self: self._sf.sftype('TimeCard__c').fromcase(self)
+SFCaseObject.timecards = lambda self, **kwargs: self._sf.sftype('TimeCard__c').fromcase(self, **kwargs)
+
+# Extend SFUserObject with timecards()
+SFUserObject.timecards = lambda self, **kwargs: self._sf.sftype('TimeCard__c').fromuser(self, **kwargs)
 
 
 class SFTimeCardType(SFType, name='TimeCard__c'):
     '''SF TimeCard Type.'''
-    def fromcase(self, case):
-        return self.query(f"CaseId__c = '{case.Id}'")
+    def fromcase(self, case, **kwargs):
+        return self.query(f"CaseId__c = '{case.Id}'", **kwargs)
+
+    def fromuser(self, user, **kwargs):
+        return self.query(f"OwnerId__c = '{user.Id}'", **kwargs)
+
+    def query(self, where, *, before=None, after=None, smaller=None, larger=None, **kwargs):
+        if before:
+            where = WhereUtil.AND(where, f"EndTime__c <= {before.isoformat()}")
+        if after:
+            where = WhereUtil.AND(where, f"StartTime__c >= {after.isoformat()}")
+        if larger:
+            where = WhereUtil.AND(where, f"TotalMinutesStatic__c >= {larger}")
+        if smaller:
+            where = WhereUtil.AND(where, f"TotalMinutesStatic__c <= {smaller}")
+        return super().query(where, **kwargs)
 
 
 class SFTimeCardObject(SFObject, name='TimeCard__c'):
